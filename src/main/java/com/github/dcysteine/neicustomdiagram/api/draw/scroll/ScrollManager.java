GTNewHorizons/nei-custom-diagram

commit 12c8229d1b5e68b4b4c518734e3827d92521aaad

remove reflection for AT's (#42)

src/main/java/com/github/dcysteine/neicustomdiagram/api/draw/scroll/ScrollManager.java
@@ -12,7 +12,6 @@ import org.lwjgl.opengl.GL11;
 
 import com.github.dcysteine.neicustomdiagram.api.draw.Dimension;
 import com.github.dcysteine.neicustomdiagram.api.draw.Point;
-import com.github.dcysteine.neicustomdiagram.main.Reflection;
 import com.github.dcysteine.neicustomdiagram.main.config.ConfigOptions;
 
 import codechicken.lib.gui.GuiDraw;
@@ -113,8 +112,8 @@ public final class ScrollManager {
         java.awt.Point mouse = GuiDraw.getMousePosition();
         java.awt.Point offset = gui.getRecipePosition(recipe);
 
-        int x = mouse.x + horizontalScrollbar.getScroll() - (Reflection.GUI_LEFT.get(gui) + offset.x);
-        int y = mouse.y + verticalScrollbar.getScroll() - (Reflection.GUI_TOP.get(gui) + offset.y);
+        int x = mouse.x + horizontalScrollbar.getScroll() - gui.guiLeft + offset.x;
+        int y = mouse.y + verticalScrollbar.getScroll() - gui.guiTop + offset.y;
         return Point.create(x, y);
     }
 
@@ -132,7 +131,7 @@ public final class ScrollManager {
         }
         GuiRecipe<?> gui = guiOptional.get();
 
-        return Point.create(Reflection.GUI_LEFT.get(gui) + SIDE_MARGIN, Reflection.GUI_TOP.get(gui) + TOP_MARGIN);
+        return Point.create(gui.guiLeft + SIDE_MARGIN, gui.guiTop + TOP_MARGIN);
     }
 
     Dimension getViewportDimension() {
@@ -143,9 +142,7 @@ public final class ScrollManager {
         }
         GuiRecipe<?> gui = guiOptional.get();
 
-        return Dimension.create(
-                Reflection.X_SIZE.get(gui) - 2 * SIDE_MARGIN,
-                Reflection.Y_SIZE.get(gui) - (TOP_MARGIN + BOTTOM_MARGIN));
+        return Dimension.create(gui.xSize - 2 * SIDE_MARGIN, gui.ySize - (TOP_MARGIN + BOTTOM_MARGIN));
     }
 
     private void setScissor() {
@@ -162,9 +159,7 @@ public final class ScrollManager {
         final int guiLeft = (int) matBuf.get(12);
         final int guiTop = (int) matBuf.get(13);
         final int left = guiLeft + SIDE_MARGIN + SCISSOR_MODELVIEW_OFFSET_X;
-        final int bottom = gui.height - (guiTop + Reflection.Y_SIZE.get(gui))
-                + BOTTOM_MARGIN
-                + SCISSOR_MODELVIEW_OFFSET_Y;
+        final int bottom = gui.height - (guiTop + gui.ySize) + BOTTOM_MARGIN + SCISSOR_MODELVIEW_OFFSET_Y;
 
         final Minecraft minecraft = Minecraft.getMinecraft();
         ScaledResolution res = new ScaledResolution(minecraft, minecraft.displayWidth, minecraft.displayHeight);

gradle.properties
@@ -80,7 +80,7 @@ apiPackage = api
 # Specify the configuration file for Forge's access transformers here. It must be placed into /src/main/resources/META-INF/
 # There can be multiple files in a space-separated list.
 # Example value: mymodid_at.cfg nei_at.cfg
-accessTransformersFile =
+accessTransformersFile = neicustomdiagram_at.cfg
 
 # Provides setup for Mixins if enabled. If you don't know what mixins are: Keep it disabled!
 usesMixins = false

src/main/java/com/github/dcysteine/neicustomdiagram/main/Reflection.java
@@ -1,52 +1,0 @@
-package com.github.dcysteine.neicustomdiagram.main;
-
-import java.lang.reflect.Field;
-
-import net.minecraft.client.gui.inventory.GuiContainer;
-
-import com.google.auto.value.AutoValue;
-
-import cpw.mods.fml.relauncher.ReflectionHelper;
-
-/** Class containing reflection accessors for private fields. */
-public final class Reflection {
-
-    public static final ReflectionField<GuiContainer, Integer> GUI_LEFT = ReflectionField
-            .createInteger(GuiContainer.class, "guiLeft", "field_147003_i");
-    public static final ReflectionField<GuiContainer, Integer> GUI_TOP = ReflectionField
-            .createInteger(GuiContainer.class, "guiTop", "field_147009_r");
-    public static final ReflectionField<GuiContainer, Integer> X_SIZE = ReflectionField
-            .createInteger(GuiContainer.class, "xSize", "field_146999_f");
-    public static final ReflectionField<GuiContainer, Integer> Y_SIZE = ReflectionField
-            .createInteger(GuiContainer.class, "ySize", "field_147000_g");
-
-    @AutoValue
-    public abstract static class ReflectionField<T, U> {
-
-        public static <T> ReflectionField<T, Integer> createInteger(Class<T> clazz, String... fieldNames) {
-            Field field = ReflectionHelper.findField(clazz, fieldNames);
-            return new AutoValue_Reflection_ReflectionField<>(field, field::getInt);
-        }
-
-        @FunctionalInterface
-        protected interface FieldAccessorFunction<R, S> {
-
-            S apply(R obj) throws IllegalAccessException;
-        }
-
-        public abstract Field field();
-
-        public abstract FieldAccessorFunction<T, U> accessorFunction();
-
-        public U get(T obj) {
-            try {
-                return accessorFunction().apply(obj);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Could not access reflection field!", e);
-            }
-        }
-    }
-
-    // Static class.
-    private Reflection() {}
-}

src/main/resources/META-INF/neicustomdiagram_at.cfg
@@ -1,0 +1,4 @@
+public net.minecraft.client.gui.inventory.GuiContainer field_147003_i #guiLeft
+public net.minecraft.client.gui.inventory.GuiContainer field_147009_r #guitop
+public net.minecraft.client.gui.inventory.GuiContainer field_146999_f #xSize
+public net.minecraft.client.gui.inventory.GuiContainer field_147000_g #ySize

commit 3b54cef2825fc142d6419fa185884dc8d96081f6

fix bug introduced in #42

src/main/java/com/github/dcysteine/neicustomdiagram/api/draw/scroll/ScrollManager.java
@@ -112,8 +112,8 @@ public final class ScrollManager {
         java.awt.Point mouse = GuiDraw.getMousePosition();
         java.awt.Point offset = gui.getRecipePosition(recipe);
 
-        int x = mouse.x + horizontalScrollbar.getScroll() - gui.guiLeft + offset.x;
-        int y = mouse.y + verticalScrollbar.getScroll() - gui.guiTop + offset.y;
+        int x = mouse.x + horizontalScrollbar.getScroll() - (gui.guiLeft + offset.x);
+        int y = mouse.y + verticalScrollbar.getScroll() - (gui.guiTop + offset.y);
         return Point.create(x, y);
     }
 